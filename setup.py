b0ef5eaef133292895fc6a108623925ddd552270
MrDeveloper <alireza2020abbasian@gmail.com>
2022-05-24 18:14:56 +0430

    fixer-demo version 0.2

--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@ from setuptools import setup
 
 setup(
     name='fixer-demo',
-    version='0.1',
+    version='0.2',
     description='Fixer service demo package',
     url='https://gitlab.com/MrDeveloper-2020/fixer-demo.git',
     author='Alireza',
     author_email='Alireza2020abbasian@gmail.com',
     license='MIT',
     packge=['fixer'],
+    install_requires=['requests'],
     zip_safe=False
 )
